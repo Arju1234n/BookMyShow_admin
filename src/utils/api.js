41bb4854017f608d03cb4e3a9218800819a640b8
arjun kumar <kumararjun5230@gmail.com>
2026-04-13 22:05:24 +0530

    Update API URL to deployed Vercel backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://book-my-show-backend-kohl.vercel.app',
 });
 
 // Attach token to every request
